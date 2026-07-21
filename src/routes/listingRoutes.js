6b9f6fb8b5ece57981a6bade8ebacf39bd1d03e5
propnex0 <propnex0@gmail.com>
2026-07-21 16:13:42 +0530

    Update listing routes

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const upload = require("../middleware/uploadMiddleware");
-const { protect } = require("../middleware/authMiddleware");
+
+const protect = require("../middleware/authMiddleware");
 
 const {
   createListing,
