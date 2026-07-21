760642975e7331bbb888094544e8b0844892d5ba
propnex0 <propnex0@gmail.com>
2026-07-21 17:59:56 +0530

    fix cors and

--- src/server.js
+++ src/server.js
@@ -11,11 +11,11 @@ const agencyRoutes = require("./routes/agencyRoutes");
 const leadRoutes = require("./routes/leadRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 
-
 const app = express();
 
 
-// MongoDB
+// MongoDB Connect
+
 connectDB();
 
 
@@ -23,57 +23,58 @@ connectDB();
 
 app.use(
   cors({
-    origin: [
-      "https://prop-nex-frontend-fd2aw0fhe-prop-nex1.vercel.app",
+    origin:[
+      "http://localhost:5173",
       "https://prop-nex-frontend.vercel.app"
     ],
-    credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "OPTIONS"
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization"
-    ]
+    credentials:true
   })
 );
 
 
-app.options("*", cors());
-
-
-// Body
+// Body Parser
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(express.urlencoded({ extended: true }));
+
+
+// Static Files
+
+app.use(
+  "/uploads",
+  express.static("uploads")
+);
 
 
 // Routes
 
 app.use("/api/auth", authRoutes);
-
-app.use("/api/listings", listingRoutes);
-
-app.use("/api/agency", agencyRoutes);
-
 app.use("/api/leads", leadRoutes);
-
+app.use("/api/listings", listingRoutes);
+app.use("/api/agency", agencyRoutes);
 app.use("/api/payment", paymentRoutes);
 
 
+// Home Route
 
-app.get("/",(req,res)=>{
- res.json({
-  success:true,
-  message:"PropNex API Running"
- });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "PropNex API Running"
+  });
 });
 
+
+// 404 Route
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found"
+  });
+});
+
+
+// Export for Vercel
 
 module.exports = app;
